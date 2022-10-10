f6d35d1833294242830e4a4cd2c94672fa065cb9
jochenruland <jochenhgruland@gmail.com>
2022-10-10 08:28:32 +0200

    EDIT constructor parameters in deployment script

--- migrations/2_elefantVonHintenDeploy.js
+++ migrations/2_elefantVonHintenDeploy.js
@@ -1,5 +1,5 @@
 const ElefantVonHinten = artifacts.require("ElefantVonHinten");
 
 module.exports = function (deployer) {
-  deployer.deploy(ElefantVonHinten, 'ElefantVonHinten', 'EVH', 'www.elefantvonhinten.de', 5, 2, 1000);
+  deployer.deploy(ElefantVonHinten, 'ElefantVonHinten', 'EVH');
 };
